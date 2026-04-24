b0e11915b4e4b2870e4a015b2eb3a5775453fef9
megacheesecake09-prog <megacheesecake09@gmail.com>
2026-04-24 21:47:56 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "french-vocab-tester-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./data.js",
+  "./manifest.webmanifest",
+  "./icon.svg",
+  "./icon-192.png",
+  "./icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request)),
+  );
+});
